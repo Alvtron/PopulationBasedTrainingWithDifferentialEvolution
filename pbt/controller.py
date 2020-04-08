15762aa48b28f1c6e3ca0cd3f5385b5d3bf695dd
Thomas Angeland <thomas.angeland@gmail.com>
2020-04-08 15:40:41 +0200

    Made functions in controller private.

--- pbt/controller.py
+++ pbt/controller.py
@@ -66,7 +66,7 @@ class Controller(object):
         nfe = f"({self.nfe}/{self.end_criteria['nfe']})" if 'nfe' in self.end_criteria and self.end_criteria['nfe'] else self.nfe
         return f"{time} {nfe} {generation}{f' {tag}' if tag else ''}: {message}"
 
-    def _say(self, message : str, member : Checkpoint = None) -> None:
+    def __say(self, message : str, member : Checkpoint = None) -> None:
         """Prints the provided controller message in the appropriate syntax if verbosity level is above 0."""
         self.__register(message=message, verbosity=0, member=member)
 
@@ -98,7 +98,7 @@ class Controller(object):
             return
         self._tensorboard_writer.add_text(tag=tag, text_string=message, global_step=global_steps)
 
-    def _update_tensorboard(self, member : Checkpoint) -> None:
+    def __update_tensorboard(self, member : Checkpoint) -> None:
         """Plots member data to tensorboard"""
         if not self._tensorboard_writer:
             return       
@@ -122,7 +122,7 @@ class Controller(object):
                 scalar_value=hparam.normalized if isinstance(hparam, DiscreteHyperparameter) else hparam.value,
                 global_step=member.steps)
 
-    def create_member(self, id) -> Checkpoint:
+    def __create_member(self, id) -> Checkpoint:
         """Create a member object"""
         # create new member object
         member = Checkpoint(
@@ -135,25 +135,25 @@ class Controller(object):
         self.evolver.on_spawn(member, self._whisper)
         return member
 
-    def create_members(self, k : int) -> List[Checkpoint]:
+    def __create_members(self, k : int) -> List[Checkpoint]:
         members = list()
         for id in range(k):
-            members.append(self.create_member(id))
+            members.append(self.__create_member(id))
         return members
 
-    def update_database(self, member : Checkpoint) -> None:
+    def __update_database(self, member : Checkpoint) -> None:
         """Updates the database stored in files."""
         self._whisper(f"updating member {member.id} in database...")
         self.database.update(member.id, member.steps, member)
 
-    def is_member_finished(self, member : Checkpoint) -> bool:
+    def __is_member_finished(self, member : Checkpoint) -> bool:
         """With the end_criteria, check if the provided member is finished training."""
         if 'steps' in self.end_criteria and self.end_criteria['steps'] and member.steps >= self.end_criteria['steps']:
             # the number of steps is equal or above the given treshold
             return True
         return False
     
-    def is_population_finished(self) -> bool:
+    def __is_population_finished(self) -> bool:
         """
         With the end_criteria, check if the entire population is finished
         by inspecting the provided member.
@@ -162,18 +162,18 @@ class Controller(object):
             return True
         if 'score' in self.end_criteria and self.end_criteria['score'] and any(member >= self.end_criteria['score'] for member in self.population.current):
             return True
-        if all(self.is_member_finished(member) for member in self.population.current):
+        if all(self.__is_member_finished(member) for member in self.population.current):
             return True
         return False
 
-    def create_initial_generation(self) -> Generation:
-        new_members = self.create_members(k=self.population_size)
+    def __create_initial_generation(self) -> Generation:
+        new_members = self.__create_members(k=self.population_size)
         generation = Generation()
         for member in self.training_service.train(new_members, self.step_size, None, False, False):
             # log performance
-            self._say(member.performance_details(), member)
+            self.__say(member.performance_details(), member)
             # Save member to database directory.
-            self.update_database(member)
+            self.__update_database(member)
             generation.append(member)
         return generation
 
@@ -182,21 +182,21 @@ class Controller(object):
         Start global training procedure. Ends when end_criteria is met.
         """
         try:
-            self._on_start()
+            self.__on_start()
             # start controller loop
-            self._say("Starting training procedure...")
+            self.__say("Starting training procedure...")
             if not use_old:
-                self.train_synchronously()
+                self.__train_synchronously()
             else:
-                self.train_synchronously_old()
+                self.__train_synchronously_old()
             # terminate worker processes
-            self._say("finished.")
+            self.__say("finished.")
         except KeyboardInterrupt:
-            self._say("interupted.")
+            self.__say("interupted.")
         finally:
-            self._on_end()
+            self.__on_end()
 
-    def _on_start(self) -> None:
+    def __on_start(self) -> None:
         """Resets class properties, starts training service and cleans up temporary files."""
         # reset class properties
         self.nfe = 0
@@ -205,20 +205,20 @@ class Controller(object):
         # start training service
         self.training_service.start()
 
-    def _on_end(self) -> None:
+    def __on_end(self) -> None:
         """Stops training service and cleans up temporary files."""
         # close training service
         self.training_service.stop()
 
-    def train_synchronously(self) -> None:
+    def __train_synchronously(self) -> None:
         """
         Performs the training of the population synchronously.
         Each member is trained individually and asynchronously,
         but they are waiting for each other between each generation cycle.
         """
-        self._say("Training initial generation...")
-        self.population.append(self.create_initial_generation())
-        while not self.is_population_finished():
+        self.__say("Training initial generation...")
+        self.population.append(self.__create_initial_generation())
+        while not self.__is_population_finished():
             self._whisper("on generation start")
             self.evolver.on_generation_start(self.population.current, self._whisper)
             # create new generation
@@ -231,11 +231,11 @@ class Controller(object):
                 member = self.evolver.on_evaluate(candidates, self._whisper)
                 self.nfe += 1 #if isinstance(candidates, Checkpoint) else len(candidates)
                 # log performance
-                self._say(member.performance_details(), member)
+                self.__say(member.performance_details(), member)
                 # Save member to database directory.
-                self.update_database(member)
+                self.__update_database(member)
                 # write to tensorboard if enabled
-                self._update_tensorboard(member)
+                self.__update_tensorboard(member)
                 # Add member to generation.
                 new_generation.append(member)
                 self._whisper("awaiting next trained member...")
@@ -246,17 +246,17 @@ class Controller(object):
             # perform garbage collection
             self._whisper("performing garbage collection...")
             self.garbage_collector.collect(self.population.generations)
-        self._say(f"end criteria has been reached.")
+        self.__say(f"end criteria has been reached.")
 
-    def train_synchronously_old(self, eval_steps=8) -> None:
+    def __train_synchronously_old(self, eval_steps=8) -> None:
         """
         Performs the training of the population synchronously.
         Each member is trained individually and asynchronously,
         but they are waiting for each other between each generation cycle.
         """
-        self._say("Training initial generation...")
-        self.population.append(self.create_initial_generation())
-        while not self.is_population_finished():
+        self.__say("Training initial generation...")
+        self.population.append(self.__create_initial_generation())
+        while not self.__is_population_finished():
             self._whisper("on generation start")
             self.evolver.on_generation_start(self.population.current, self._whisper)
             # create new generation
@@ -272,11 +272,11 @@ class Controller(object):
             # train best candidate on full dataset
             for member in self.training_service.train(best_candidates, self.step_size - eval_steps, None, False, False):
                 # log performance
-                self._say(member.performance_details(), member)
+                self.__say(member.performance_details(), member)
                 # Save member to database directory.
-                self.update_database(member)
+                self.__update_database(member)
                 # write to tensorboard if enabled
-                self._update_tensorboard(member)
+                self.__update_tensorboard(member)
                 # Add member to generation.
                 new_generation.append(member)
                 self._whisper("awaiting next trained member...")
@@ -287,4 +287,4 @@ class Controller(object):
             # perform garbage collection
             self._whisper("performing garbage collection...")
             self.garbage_collector.collect(self.population.generations)
-        self._say(f"end criteria has been reached.")
+        self.__say(f"end criteria has been reached.")
