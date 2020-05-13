03d671fe309213805a5e018758739041e1e9856e
Thomas Angeland <thomas.angeland@gmail.com>
2020-05-13 15:54:49 +0200

    Set lower bounds to 0.0.

--- pbt/task/cifar.py
+++ pbt/task/cifar.py
@@ -45,9 +45,9 @@ class Cifar10(Task):
         return Hyperparameters(
             model= None,
             optimizer={
-                'lr': ContiniousHyperparameter(1e-9, 1e-1),
-                'momentum': ContiniousHyperparameter(1e-9, 1.0),
-                'weight_decay': ContiniousHyperparameter(1e-9, 1e-2)
+                'lr': ContiniousHyperparameter(0.0, 1e-1),
+                'momentum': ContiniousHyperparameter(0.0, 1.0),
+                'weight_decay': ContiniousHyperparameter(0.0, 1e-2)
             })
 
     @property
