088af332cc65b963433e43991e658931f4406de7
Thomas Angeland <thomas.angeland@gmail.com>
2020-06-11 15:02:49 +0200

    Improved typing and added get first or last.

--- pbt/database.py
+++ pbt/database.py
@@ -5,7 +5,7 @@ import copy
 import time
 from pathlib import Path
 from datetime import datetime
-from typing import Dict, Union, Callable, TypeVar
+from typing import Any, Dict, Union, Callable, TypeVar, Generator
 
 Entity = TypeVar('Entity')
 
@@ -27,25 +27,25 @@ class ReadOnlyDatabase(object):
         self.read = read if not read_function else read_function
 
     @property
-    def exists(self):
+    def exists(self) -> bool:
         return self.path.is_dir()
 
-    def __len__(self):
+    def __len__(self) -> int:
         entries_path = Path(self.path, ReadOnlyDatabase.ENTRIES_TAG)
         return len(list(entries_path.glob(f'**/*.{self.extension}')))
 
-    def __iter__(self):
+    def __iter__(self) -> Generator[Entity, None, None]:
         for directory in self.entry_directories():
             for entry in self.entries_from_path(directory):
                 yield entry
 
-    def __contains__(self, uid):
+    def __contains__(self, uid: Any):
         return self.create_entry_directoy_path(uid).exists()
 
-    def create_entry_file_name(self, key):
+    def create_entry_file_name(self, key: Any):
         return f"{key}.{self.extension}"
 
-    def create_entry_directoy_path(self, uid):
+    def create_entry_directoy_path(self, uid: Any):
         """ Creates a new entry directory file path. """
         return Path(self.path, ReadOnlyDatabase.ENTRIES_TAG, str(uid))
 
@@ -55,23 +55,23 @@ class ReadOnlyDatabase(object):
         entry_file_name = self.create_entry_file_name(key)
         return Path(entry_directory, entry_file_name)
 
-    def entry(self, uid, key):
+    def entry(self, uid: Any, key: Any) -> Entity:
         """ Returns the specific entry stored on the specified uid. If there is no match, None is returned. """
         entry_file_path = self.create_entry_file_path(uid, key)
         return self.read(entry_file_path) if entry_file_path.is_file() else None
 
-    def entries(self, uid):
+    def entries(self, uid: Any) -> Generator[Entity, None, None]:
         """ Iterate over the entry directory matching the specified uid and yield all entries inside the directory. """
         entry_directory = self.create_entry_directoy_path(uid)
         for content in entry_directory.glob(f"*.{self.extension}"):
             yield self.read(content)
 
-    def entry_directories(self):
+    def entry_directories(self) -> Generator[Path, None, None]:
         entries_path = Path(self.path, ReadOnlyDatabase.ENTRIES_TAG)
         for content in entries_path.iterdir():
             if content.is_dir(): yield content
 
-    def entries_from_path(self, entry_directory_path):
+    def entries_from_path(self, entry_directory_path: Path) -> Generator[Entity, None, None]:
         """ Retrieve all entries made on the specified uid. """
         for content in entry_directory_path.glob(f"*.{self.extension}"):
             yield self.read(content)
@@ -85,7 +85,25 @@ class ReadOnlyDatabase(object):
                 result[directory.name].append(key_path.stem)
         return result
 
-    def to_dict(self):
+    def get_first(self) -> Generator[Entity, None, None]:
+        """ Returns an iterator over the first entries in this database """
+        min_steps = min(min(int(key) for key in keys) for keys in self.database.identy_records().values())
+        for uid, keys in self.database.identy_records().items():
+            min_key = min(int(key) for key in keys)
+            if min_key > min_steps:
+                continue
+            yield self.database.entry(uid, min_key)
+
+    def get_last(self) -> Generator[Entity, None, None]:
+        """ Returns an iterator over the last entries in this database """
+        max_steps = max(max(int(key) for key in keys) for keys in self.identy_records().values())
+        for uid, keys in self.identy_records().items():
+            max_key = max(int(key) for key in keys)
+            if max_key < max_steps:
+                continue
+            yield self.entry(uid, max_key)
+
+    def to_dict(self) -> dict:
         """ Returns a the database converted to a dictionary grouped by uid/filename/entry"""
         dict_of_entries = dict()
         for directory in self.entry_directories():
@@ -96,13 +114,16 @@ class ReadOnlyDatabase(object):
                 dict_of_entries[directory.name][entry.steps] = entry
         return dict_of_entries
 
-    def print(self):
+    def print(self) -> None:
         """ Prints all entries in the database. """
         for entry in sorted(self, key=lambda x: (x.uid, x.steps), reverse=False):
             print(entry)
 
 class Database(ReadOnlyDatabase):
-    def __init__(self, directory_path: Union[Path, str], database_name: str = None, read_function: Callable[[str], Entity] = None, write_function: Callable[[Entity], None] = None, extension: str = "obj"):
+    def __init__(
+            self, directory_path: Union[Path, str], database_name: str = None,
+            read_function: Callable[[str], Entity] = None, write_function: Callable[[Entity], None] = None,
+            extension: str = "obj"):
         if not isinstance(directory_path, (Path, str)):
             raise TypeError(f"the 'directory_path' specified was of wrong type {type(directory_path)}, expected {Path} or {str}.")
         if database_name is not None and not isinstance(database_name, str):
@@ -127,19 +148,19 @@ class Database(ReadOnlyDatabase):
         def write(entry, path): pickle.dump(entry, path.open('wb'))
         self.write = write if not write_function else write_function
 
-    def create_folder(self, name):
+    def create_folder(self, name: str) -> Path:
         """ Create a new folder located in the database base directory. Name supports nested directories of type dir/sub_dir/sub_sub_dir/etc. """
         folder_path = Path(self.path, name)
         folder_path.mkdir(parents=True, exist_ok=True)
         return folder_path
 
-    def create_file(self, tag, file_name):
+    def create_file(self, tag: str, file_name: str) -> Path:
         """ Create a new file in a folder named after the specified tag-string, which is located in the database directory. """
         file_path = Path(self.path, tag, file_name)
         file_path.parent.mkdir(parents=True, exist_ok=True)
         return file_path
 
-    def update(self, uid, key, entry):
+    def update(self, uid: Any, key: Any, entry: Entity) -> None:
         """ Save the provided database entry to a file on uid/key inside the database directory. """
         entry_file_path = self.create_entry_file_path(uid, key)
         entry_file_path.parent.mkdir(parents=True, exist_ok=True)
